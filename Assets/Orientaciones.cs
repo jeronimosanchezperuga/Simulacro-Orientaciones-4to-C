98b157cfb5bb73786f189abc25e49730db992cf0
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-04-15 09:02:04 -0300

    Orianteacion codigo hecho

---
+++ Assets/Orientaciones.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientaciones : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int anio;
+    public string codigoOrientacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno == "")
+        {
+            Debug.Log("EL nombre no puede estar vacío");
+            return;
+        }
+        if (anio < 1 || anio > 5)
+        {
+            Debug.Log("Año ingresado no válido");
+            return;
+        }
+        if (codigoOrientacion != "T" && codigoOrientacion != "M" && 
+            codigoOrientacion != "G" && codigoOrientacion != "H" && codigoOrientacion != "D")
+        {
+            Debug.Log("Solo puede ingresar T, D, G, M o H");
+            return;
+        }
+        if (anio < 3)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico");
+            return;
+        }
+
+        Debug.Log("Muchas gracias " + nombreAlumno + "!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
